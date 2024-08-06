42669e8b3b37c6502f5c827edc99934fb5ce403e
JhonZuluaga007 <jhon.zuluaga@scrummers.co>
2024-08-06 14:51:43 -0500

    [Feature/Update] Se actualiza la forma de como se consumen los servicios de http

--- lib/infrastructure/core/remote_data_sources/http.dart
+++ lib/infrastructure/core/remote_data_sources/http.dart
@@ -1,51 +1,80 @@
-import 'package:http/http.dart';
+import 'dart:io';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 class HttpDataSource {
-  Future<Response> httpGet(
-    String url, {
-    String? token,
-  }) async {
-    Map<String, String> headers = {};
-    if (token != null) headers['Authorization'] = 'Bearer $token';
-    return await get(Uri.parse(url), headers: headers);
+  static final Map<String, String> _headers = {};
+
+  static Future<void> setHeaders() async {
+    _headers[HttpHeaders.contentTypeHeader] = 'application/json';
   }
 
-  Future<Response> httpPost(
-    String url, {
-    String? body,
-    String? token,
-  }) async {
-    Map<String, String> headers = {
-      'Content-type': 'application/json',
-      'Accept': 'application/json',
-    };
-    if (token != null) headers['Authorization'] = 'Bearer $token';
-    return await post(Uri.parse(url), headers: headers, body: body);
+  // Parsea un json en formato string a un Map
+  // Returns. Map String, dynamic
+  static dynamic decode(String response) => json.decode(response);
+
+  // Encode un json en formato Map a un String
+  // Returns. Map String, dynamic
+  static String encode(Map<String, dynamic> response) => json.encode(response);
+
+  //Realiza peticiones tipo GET al backend al endpoint pasado por parámetro
+  //Returns Future con el resultado de la consulta
+  static Future<dynamic> get(String path) async {
+    await setHeaders();
+    Uri uri = Uri.parse(path);
+    final response = await http.get(
+      uri,
+      headers: _headers,
+    );
+    return decode(response.body);
   }
 
-  Future<Response> httpPut(
-    String url, {
-    required String body,
-    String? token,
-  }) async {
-    Map<String, String> headers = {
-      'Content-type': 'application/json',
-      'Accept': 'application/json',
-    };
-    if (token != null) headers['Authorization'] = 'Bearer $token';
-    return await put(Uri.parse(url), headers: headers, body: body);
+  Future<dynamic> post(String path, Map<String, dynamic> data) async {
+    await setHeaders();
+    final body = encode(data);
+    Uri uri = Uri.parse(path);
+    try {
+      final response = await http.post(
+        uri,
+        body: body,
+        headers: _headers,
+      );
+      return response;
+    } on Exception catch (e) {
+      throw Exception(e);
+    }
   }
 
-  Future<Response> httpDelete(
-    String url, {
-    String? body,
-    String? token,
-  }) async {
-    Map<String, String> headers = {
-      'Content-type': 'application/json',
-      'Accept': 'application/json',
-    };
-    if (token != null) headers['Authorization'] = 'Bearer $token';
-    return await delete(Uri.parse(url), headers: headers, body: body);
+  static Future<dynamic> put(String path, Map<String, dynamic> data) async {
+    await setHeaders();
+    final body = encode(data);
+    Uri uri = Uri.parse(path);
+    try {
+      final response = await http.put(
+        uri,
+        body: body,
+        headers: _headers,
+      );
+      return decode(response.body);
+    } on Exception catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  static Future<dynamic> delete(String path, Map<String, dynamic> data) async {
+    await setHeaders();
+    final body = encode(data);
+    Uri uri = Uri.parse(path);
+    try {
+      final response = await http.delete(
+        uri,
+        body: body,
+        headers: _headers,
+      );
+      return decode(response.body);
+    } on Exception catch (e) {
+      throw Exception(e);
+    }
   }
 }
