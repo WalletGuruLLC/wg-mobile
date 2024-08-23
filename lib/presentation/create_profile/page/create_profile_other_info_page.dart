1fb3b8d62ae492276a79644b500627ea2df4cdaf
Cristian Vargas Scrummers <cristian.vargas@scrummers.co>
2024-08-22 21:04:44 -0400

    [Feature/add] se agregan pantallas para create profile y widgest reusables para las mismas

---
+++ lib/presentation/create_profile/page/create_profile_other_info_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:wallet_guru/presentation/core/widgets/layout.dart';
+import 'package:wallet_guru/presentation/create_wallet/widgets/create_wallet.dart';
+
+class CreateProfileOtherInfoPage extends StatelessWidget {
+  const CreateProfileOtherInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return WalletGuruLayout(
+      showSafeArea: true,
+      showAppBar: false,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+          child: SizedBox(
+            width: size.width * 0.90,
+            height: size.height,
+            child: const CreateWalletForm(),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/presentation/create_profile/widgets/create_profile_basic_info_form.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:wallet_guru/presentation/core/assets/assets.dart';
+import 'package:wallet_guru/presentation/core/widgets/base_modal.dart';
+import 'package:wallet_guru/presentation/core/widgets/text_base.dart';
+import 'package:wallet_guru/presentation/core/widgets/custom_button.dart';
+import 'package:wallet_guru/presentation/core/widgets/auth_login_divider.dart';
+import 'package:wallet_guru/presentation/core/styles/schemas/app_color_schema.dart';
+import 'package:wallet_guru/presentation/core/widgets/forms/wallet_address_form.dart';
+import 'package:wallet_guru/presentation/core/widgets/user_profile_description.dart';
+
+class CreateProfileBasicInfoForm extends StatefulWidget {
+  const CreateProfileBasicInfoForm({super.key});
+
+  @override
+  State<CreateProfileBasicInfoForm> createState() =>
+      CreateProfileBasicInfoFormState();
+}
+
+class CreateProfileBasicInfoFormState
+    extends State<CreateProfileBasicInfoForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _address = '';
+  bool _addressMinLength = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    double size = MediaQuery.of(context).size.height;
+    return Form(
+        key: _formKey, child: _buildEmailAndPasswordView(size, context, l10n));
+  }
+
+  Widget _buildEmailAndPasswordView(
+      double size, BuildContext context, AppLocalizations l10n) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const UserProfileDescription(),
+          SizedBox(height: size * 0.030),
+          TextBase(
+            text: l10n.enterDesiredName,
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          SizedBox(height: size * 0.030),
+          TextBase(
+            text: l10n.addressName,
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          SizedBox(height: size * 0.015),
+          WalletAddressForm(
+            initialValue: _address,
+            onChanged:
+                _onFormChanged, // Call the corrected _onFormChanged method
+          ),
+          SizedBox(height: size * 0.35),
+          CustomButton(
+            border: Border.all(
+                color: !_addressMinLength
+                    ? AppColorSchema.of(context).secondaryButtonBorderColor
+                    : Colors.transparent),
+            color: !_addressMinLength
+                ? Colors.transparent
+                : AppColorSchema.of(context).buttonColor,
+            text: l10n.verify,
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            onPressed: _onButtonPressed,
+          ),
+          SizedBox(height: size * 0.025),
+        ],
+      ),
+    );
+  }
+
+  // Method to handle form changes
+  void _onFormChanged(String? value) {
+    setState(() {
+      _address = value!;
+      _addressMinLength = value.length > 4;
+    });
+  }
+
+  // Method to handle button actions
+  void _onButtonPressed() {
+    if (_formKey.currentState!.validate() && _addressMinLength) {
+      _formKey.currentState!.reset();
+      setState(() {
+        _address = '';
+        _addressMinLength = false;
+      });
+      _buildSuccessfulModal();
+    }
+  }
+
+  // Method to build the successful modal
+  Future<dynamic> _buildSuccessfulModal() {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        final l10n = AppLocalizations.of(context)!;
+        double size = MediaQuery.of(context).size.height;
+        return BaseModal(
+          content: Column(
+            children: [
+              SizedBox(height: size * 0.025),
+              TextBase(
+                textAlign: TextAlign.center,
+                text: l10n.walletSuccessMessage,
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color: AppColorSchema.of(context).secondaryText,
+              ),
+              SizedBox(height: size * 0.025),
+              TextBase(
+                textAlign: TextAlign.center,
+                text: l10n.continueCheckingProfile,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: AppColorSchema.of(context).secondaryText,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
