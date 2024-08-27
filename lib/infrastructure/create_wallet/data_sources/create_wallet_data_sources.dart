02f4e180df7a2dae00f19942aededc77310f5763
Cristian Vargas <cristian.vargas@scrummers.co>
2024-08-26 20:20:16 -0400

    [Feature/add] se corrgie nombre en createWalletNetwork

--- lib/infrastructure/create_wallet/data_sources/create_wallet_data_sources.dart
+++ lib/infrastructure/create_wallet/data_sources/create_wallet_data_sources.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:wallet_guru/domain/core/models/invalid_data.dart';
 import 'package:wallet_guru/domain/core/models/response_model.dart';
 import 'package:wallet_guru/infrastructure/create_wallet/network/create_wallet_network.dart';
-import 'package:wallet_guru/infrastructure/login/network/login_network.dart';
+
 import 'package:wallet_guru/infrastructure/core/remote_data_sources/http.dart';
 
 class CreateWalletDataSource {
--- lib/infrastructure/create_wallet/network/create_wallet_network.dart
+++ lib/infrastructure/create_wallet/network/create_wallet_network.dart
@@ -1,5 +1,5 @@
 import 'package:wallet_guru/infrastructure/core/env/env.dart';
 
 class CreateWalletNetwork {
-  static final String signIn = '${Env.baseUrl}/api/v1/wallets';
+  static final String createWallet = '${Env.baseUrl}/api/v1/wallets';
 }
