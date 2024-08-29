dab32ccc631bf248a668350636bffca780c8f96b
JhonZuluaga007 <jhon.zuluaga@scrummers.co>
2024-08-29 02:39:40 -0500

    [Feature/Add] Se crea el consumo del flujo de cracion del perfil del usuario en donde en la capa del data source utilizamos un put para el flujo

---
+++ lib/domain/create_profile/repositories/create_profile_repository.dart
@@ -0,0 +1,10 @@
+import 'package:either_dart/either.dart';
+
+import 'package:wallet_guru/domain/core/models/invalid_data.dart';
+import 'package:wallet_guru/domain/core/models/response_model.dart';
+import 'package:wallet_guru/domain/create_profile/entities/base_profile_entity.dart';
+
+abstract class CreateProfileRepository {
+  Future<Either<InvalidData, ResponseModel>> updateUser<T extends BaseProfileEntity>(T entity);
+  
+}
---
+++ lib/infrastructure/create_profile/data_sources/create_profile_data_source.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:wallet_guru/domain/core/models/invalid_data.dart';
+import 'package:wallet_guru/domain/core/models/response_model.dart';
+import 'package:wallet_guru/domain/create_profile/entities/base_profile_entity.dart';
+import 'package:wallet_guru/infrastructure/core/remote_data_sources/http.dart';
+import 'package:wallet_guru/infrastructure/create_profile/network/create_profile_network.dart';
+
+class CreateProfileDataSource {
+  Future<ResponseModel> updateUser<T extends BaseProfileEntity>(
+      T entity) async {
+    var response = await HttpDataSource.put(
+      "${CreateProfileNetwork.updateUser}${entity.id}",
+      entity.toJson(),
+    );
+    final result = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      ResponseModel registerModel = ResponseModel.fromJson(result);
+      return registerModel;
+    } else {
+      final errorModel = ResponseModel.fromJson(result);
+      throw InvalidData(errorModel.customCode, errorModel.customMessage);
+    }
+  }
+}
