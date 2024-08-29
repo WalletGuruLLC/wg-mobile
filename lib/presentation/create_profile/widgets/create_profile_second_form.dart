a38a24b6c79d019eea8f5d2f2efe6f472db9e986
JhonZuluaga007 <jhon.zuluaga@scrummers.co>
2024-08-29 08:54:01 -0500

    [Feature/Update] se organiza manejo del bloc

--- lib/presentation/create_profile/widgets/create_profile_second_form.dart
+++ lib/presentation/create_profile/widgets/create_profile_second_form.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
-import 'package:wallet_guru/infrastructure/core/routes/routes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'package:wallet_guru/presentation/core/widgets/create_profile_buttons.dart';
-import 'package:wallet_guru/presentation/core/widgets/forms/dropdown_base.dart';
+import 'package:wallet_guru/infrastructure/core/routes/routes.dart';
+import 'package:wallet_guru/presentation/core/widgets/text_base.dart';
+import 'package:wallet_guru/presentation/core/widgets/base_modal.dart';
+import 'package:wallet_guru/presentation/core/widgets/progress_bar.dart';
+import 'package:wallet_guru/domain/core/models/form_submission_status.dart';
 import 'package:wallet_guru/presentation/core/widgets/forms/form_label.dart';
+//import 'package:wallet_guru/presentation/core/widgets/forms/dropdown_base.dart';
+import 'package:wallet_guru/application/create_profile/create_profile_cubit.dart';
+import 'package:wallet_guru/presentation/core/widgets/create_profile_buttons.dart';
+import 'package:wallet_guru/presentation/core/styles/schemas/app_color_schema.dart';
+import 'package:wallet_guru/presentation/core/widgets/user_profile_description.dart';
+import 'package:wallet_guru/presentation/core/widgets/forms/social_security_form.dart';
 import 'package:wallet_guru/presentation/core/widgets/forms/identification_number_form.dart';
 import 'package:wallet_guru/presentation/core/widgets/forms/identification_type_drop_form.dart';
-import 'package:wallet_guru/presentation/core/widgets/forms/social_security_form.dart';
-import 'package:wallet_guru/presentation/core/widgets/progress_bar.dart';
-import 'package:wallet_guru/presentation/core/widgets/user_profile_description.dart';
 
 class CreateProfileSecondForm extends StatefulWidget {
   const CreateProfileSecondForm({super.key});
@@ -25,6 +31,14 @@ class CreateProfileSecondFormState extends State<CreateProfileSecondForm> {
   String _ssn = '';
   String _identificationNumber = '';
   String _idType = '';
+  late CreateProfileCubit createProfileCubit;
+
+  @override
+  void initState() {
+    createProfileCubit = BlocProvider.of<CreateProfileCubit>(context);
+    createProfileCubit.cleanFormStatusOne();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,10 +65,10 @@ class CreateProfileSecondFormState extends State<CreateProfileSecondForm> {
           const SizedBox(height: 30),
           const FormLabel(label: 'Identification Type'),
           IdentificationTypeDropForm(
-            // initialValue: 'item1',
+            initialValue: _idType,
             hintText: 'Select you ID type',
-            items: const ['item1', 'item2', 'item3'],
-            onChanged: (value) {},
+            items: const ['National ID', 'Passport', 'Driver´s lisence'],
+            onChanged: (value) => _onFormChanged('idType', value),
           ),
           const SizedBox(height: 30),
           FormLabel(label: l10n.identificationNumber),
@@ -63,9 +77,24 @@ class CreateProfileSecondFormState extends State<CreateProfileSecondForm> {
             onChanged: (value) => _onFormChanged('idNumber', value),
           ),
           SizedBox(height: size * 0.12),
-          CreateProfileButtons(
-            onPressed1: _onBackButtonPressed,
-            onPressed2: _onNextButtonPressed,
+          BlocConsumer<CreateProfileCubit, CreateProfileState>(
+            listener: (context, state) {
+              if (state.formStatusOne is SubmissionSuccess) {
+                GoRouter.of(context).pushNamed(Routes.createProfile3.name);
+              } else if (state.formStatusOne is SubmissionFailed) {
+                _buildlModal(state.customMessage, state.customCode);
+              }
+            },
+            builder: (context, state) {
+              if (state.formStatusOne is FormSubmitting) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return CreateProfileButtons(
+                  onPressed1: _onBackButtonPressed,
+                  onPressed2: _onNextButtonPressed,
+                );
+              }
+            },
           ),
         ],
       ),
@@ -78,7 +107,7 @@ class CreateProfileSecondFormState extends State<CreateProfileSecondForm> {
         case 'snn':
           _ssn = value!;
           break;
-        case 'lastName':
+        case 'idType':
           _idType = value!;
           break;
         case 'idNumber':
@@ -97,10 +126,48 @@ class CreateProfileSecondFormState extends State<CreateProfileSecondForm> {
     if (_formKey.currentState?.validate() ?? false) {
       // Form is valid, proceed with further actions
       debugPrint('Form is valid');
+      //createProfileCubit.cleanFormStatusTwo();
       GoRouter.of(context).pushNamed(Routes.createProfile3.name);
     } else {
       // Form is invalid, show errors
       debugPrint('Form is invalid');
     }
+  }
+
+  // Method to build the successful modal
+  Future<dynamic> _buildlModal(String description, String codeError) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        double size = MediaQuery.of(context).size.height;
+        return BaseModal(
+          isFail: true,
+          content: Column(
+            children: [
+              SizedBox(height: size * 0.025),
+              TextBase(
+                textAlign: TextAlign.center,
+                text: description,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                color: AppColorSchema.of(context).secondaryText,
+              ),
+              SizedBox(height: size * 0.025),
+              TextBase(
+                textAlign: TextAlign.center,
+                text: codeError,
+                fontSize: 10,
+                fontWeight: FontWeight.w400,
+                color: AppColorSchema.of(context).secondaryText,
+              ),
+            ],
+          ),
+          onPressed: () {
+            createProfileCubit.cleanFormStatusOne();
+            Navigator.of(context).pop();
+          },
+        );
+      },
+    );
   }
 }
