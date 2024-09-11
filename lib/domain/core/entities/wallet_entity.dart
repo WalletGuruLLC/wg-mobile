e7152f17f6aa6fca2d8ee87158abf21745180ef4
Cristian Vargas Scrummers <cristian.vargas@scrummers.co>
2024-09-11 14:12:15 -0400

    [Feature/add] se agregan puntos exactos en los que se requiere la data del wallet para bloquear la cuenta

--- lib/domain/core/entities/wallet_entity.dart
+++ lib/domain/core/entities/wallet_entity.dart
@@ -3,19 +3,23 @@ import 'package:wallet_guru/domain/core/models/response_model.dart';
 class WalletEntity {
   final String id;
   final String walletAddress;
+  final bool active;
 
   WalletEntity({
     required this.id,
     required this.walletAddress,
+    required this.active,
   });
 
   WalletEntity copyWith({
     String? id,
     String? walletAddress,
+    bool? active,
   }) {
     return WalletEntity(
       id: id ?? this.id,
       walletAddress: walletAddress ?? this.walletAddress,
+      active: active ?? this.active,
     );
   }
 
@@ -30,6 +34,7 @@ class WalletEntity {
     return WalletEntity(
       id: wallet.id,
       walletAddress: wallet.walletAddress,
+      active: wallet.active,
     );
   }
 }
