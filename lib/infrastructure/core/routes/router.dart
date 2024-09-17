21442827bb81e9b27b5321b28e9fe3110e55a9b2
Cristian Vargas Scrummers <cristian.vargas@scrummers.co>
2024-09-17 18:02:46 -0400

    [Fix/error] se corrige error que retrocedia estado inicial del formulario

--- lib/infrastructure/core/routes/router.dart
+++ lib/infrastructure/core/routes/router.dart
@@ -25,7 +25,7 @@ class WalletGuruRouter {
     GoRoute(
       name: Routes.splash.name,
       path: Routes.splash.path,
-      builder: (context, state) => const SplashScreenPage(),
+      builder: (context, state) => const LoginPage(),
     ),
     GoRoute(
       name: Routes.signUp.name,
--- lib/presentation/change_password/page/change_password_page.dart
+++ lib/presentation/change_password/page/change_password_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:wallet_guru/infrastructure/core/routes/routes.dart';
 
 import 'package:wallet_guru/presentation/core/widgets/layout.dart';
 import 'package:wallet_guru/presentation/change_password/widgets/change_password_form.dart';
@@ -9,20 +11,27 @@ class ChangePasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return WalletGuruLayout(
-      showSafeArea: true,
-      showBackButton: false,
-      showNotLoggedAppBar: true,
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-          child: SizedBox(
-            width: size.width * 0.90,
-            height: size.height,
-            child: const ChangePasswordForm(),
+    return PopScope(
+      onPopInvoked: (didPop) {
+        if (didPop) {
+          GoRouter.of(context).pushReplacementNamed(Routes.myProfile.name);
+        }
+      },
+      child: WalletGuruLayout(
+        showSafeArea: true,
+        showBackButton: false,
+        showNotLoggedAppBar: true,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: SizedBox(
+              width: size.width * 0.90,
+              height: size.height,
+              child: const ChangePasswordForm(),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
