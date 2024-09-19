8eb14fb44eef1581ec92fe977cf3c6d02c81baae
Cristian Vargas Scrummers <cristian.vargas@scrummers.co>
2024-09-19 17:51:45 -0400

    [Feature/add] se crea vista

--- lib/presentation/payments/pages/send_payment_by_qr_page.dart
+++ lib/presentation/payments/pages/send_payment_by_qr_page.dart
@@ -1,22 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:wallet_guru/presentation/core/widgets/appbar/app_top_bar.dart';
 
-class SendPaymentsPageByQr extends StatelessWidget {
+class SendPaymentsPageByQr extends StatefulWidget {
   const SendPaymentsPageByQr({super.key});
 
   @override
+  _SendPaymentsPageByQrState createState() => _SendPaymentsPageByQrState();
+}
+
+class _SendPaymentsPageByQrState extends State<SendPaymentsPageByQr> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
   Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context)!;
-    Size size = MediaQuery.of(context).size;
     return PopScope(
-        canPop: false,
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: WalletGuruAppBar(
-            title: 'jajajajaja',
-            action: () {},
-          ),
-        ));
+      canPop: true,
+      onPopInvoked: (didPop) {},
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        extendBodyBehindAppBar: true,
+        appBar: WalletGuruAppBar(
+          title: 'xd',
+          action: () {},
+          showSimpleStyle: false,
+        ),
+        body: Stack(
+          children: [
+            _buildQrView(context), // Background QR Scanner View
+            const Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+        borderColor: Colors.white,
+        borderRadius: 10,
+        borderLength: 30,
+        borderWidth: 10,
+        cutOutSize: scanArea,
+      ),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('No permission to access camera')),
+      );
+    }
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {});
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
