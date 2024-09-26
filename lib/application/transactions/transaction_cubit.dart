92eae36af8323549d86de3f11592cd93b970bb48
JhonZuluaga007 <jhon.zuluaga@scrummers.co>
2024-09-26 13:53:46 -0500

    [Feature/Add] Se crea Cubit para las transacciones para poder manejar el estado a nivel de capa de presentacion

---
+++ lib/application/transactions/transaction_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'transaction_state.dart';
+import 'package:wallet_guru/infrastructure/core/injector/injector.dart';
+import 'package:wallet_guru/domain/transactions/repositories/transaction_repository.dart';
+
+class TransactionCubit extends Cubit<TransactionState> {
+  TransactionCubit() : super(TransactionInitial());
+  final transactionRepository = Injector.resolve<TransactionRepository>();
+
+  Future<void> loadTransactions() async {
+    emit(TransactionLoading());
+    final result = await transactionRepository.getTransactions();
+    result.fold(
+      (exception) => emit(TransactionError(message: exception.toString())),
+      (response) {
+        final payments =
+            response.data!.transactionsModel!.completedIncomingPayments;
+        emit(TransactionLoaded(payments: payments));
+      },
+    );
+  }
+}
