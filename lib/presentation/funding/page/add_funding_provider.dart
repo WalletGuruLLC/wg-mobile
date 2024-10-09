c322a6a13327b3eedc5efd57f5331b4779aceb2e
Cristian Vargas Scrummers <cristian.vargas@scrummers.co>
2024-10-09 15:39:06 -0400

    Fix scroll to new view

--- lib/presentation/funding/page/add_funding_provider.dart
+++ lib/presentation/funding/page/add_funding_provider.dart
@@ -60,7 +60,7 @@ class _AddFundsProviderState extends State<AddFundsProvider> {
           padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
           child: SizedBox(
             width: size.width * 0.90,
-            height: size.height,
+            height: size.height * 0.80,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
