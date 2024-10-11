b1ae9a963f2354d91d622e3292f2b5ad2664480a
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2024-10-11 08:02:04 -0500

    [Feature/Update] se agrega el navigatorkey, para poder extaer la instacia del contexto

--- lib/infrastructure/core/routes/router_provider.dart
+++ lib/infrastructure/core/routes/router_provider.dart
@@ -1,10 +1,14 @@
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:wallet_guru/infrastructure/core/routes/router.dart';
 import 'package:wallet_guru/infrastructure/core/routes/routes.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 final GoRouter router = GoRouter(
   initialLocation: Routes.splash.path,
   debugLogDiagnostics: true,
+  navigatorKey: navigatorKey,
   routes: WalletGuruRouter.routes,
 );
