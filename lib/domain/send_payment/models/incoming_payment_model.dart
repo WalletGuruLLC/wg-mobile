4ea9776d2d7335b4bcc391caf837124f753c1835
JhonZuluaga007 <jhonsalazar93@hotmail.com>
2024-10-15 21:48:24 -0500

    [Feature/Update] se actualiza el modelo Incoming payments

--- lib/domain/send_payment/models/incoming_payment_model.dart
+++ lib/domain/send_payment/models/incoming_payment_model.dart
@@ -1,27 +1,28 @@
 import 'dart:math';
 
 class IncomingPaymentModel {
-    String type;
-    String id;
-    String provider;
-    String ownerUser;
-    String state;
-    IncomingAmount incomingAmount;
-    DateTime createdAt;
-    DateTime expiresAt;
+  String type;
+  String id;
+  String provider;
+  String ownerUser;
+  String state;
+  IncomingAmount incomingAmount;
+  DateTime createdAt;
+  DateTime expiresAt;
 
-    IncomingPaymentModel({
-        required this.type,
-        required this.id,
-        required this.provider,
-        required this.ownerUser,
-        required this.state,
-        required this.incomingAmount,
-        required this.createdAt,
-        required this.expiresAt,
-    });
+  IncomingPaymentModel({
+    required this.type,
+    required this.id,
+    required this.provider,
+    required this.ownerUser,
+    required this.state,
+    required this.incomingAmount,
+    required this.createdAt,
+    required this.expiresAt,
+  });
 
-    factory IncomingPaymentModel.fromJson(Map<String, dynamic> json) => IncomingPaymentModel(
+  factory IncomingPaymentModel.fromJson(Map<String, dynamic> json) =>
+      IncomingPaymentModel(
         type: json["type"],
         id: json["id"],
         provider: json["provider"],
@@ -30,9 +31,9 @@ class IncomingPaymentModel {
         incomingAmount: IncomingAmount.fromJson(json["incomingAmount"]),
         createdAt: DateTime.parse(json["createdAt"]),
         expiresAt: DateTime.parse(json["expiresAt"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "type": type,
         "id": id,
         "provider": provider,
@@ -41,36 +42,35 @@ class IncomingPaymentModel {
         "incomingAmount": incomingAmount.toJson(),
         "createdAt": createdAt.toIso8601String(),
         "expiresAt": expiresAt.toIso8601String(),
-    };
+      };
 }
 
 class IncomingAmount {
-    String typename;
-    int assetScale;
-    String assetCode;
-    double value;
+  String typename;
+  int assetScale;
+  String assetCode;
+  double value;
 
-    IncomingAmount({
-        required this.typename,
-        required this.assetScale,
-        required this.assetCode,
-        required this.value,
-    });
+  IncomingAmount({
+    required this.typename,
+    required this.assetScale,
+    required this.assetCode,
+    required this.value,
+  });
 
-    factory IncomingAmount.fromJson(Map<String, dynamic> json) => IncomingAmount(
+  factory IncomingAmount.fromJson(Map<String, dynamic> json) => IncomingAmount(
         typename: json["_Typename"],
         assetScale: json["assetScale"],
         assetCode: json["assetCode"],
         value: json["value"] != null
             ? ((int.parse(json["value"])) / pow(10, json["assetScale"]))
             : 0,
-      
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "_Typename": typename,
         "assetScale": assetScale,
         "assetCode": assetCode,
         "value": value,
-    };
+      };
 }
