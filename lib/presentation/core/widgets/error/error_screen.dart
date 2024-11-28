594362bccabb2282dbe6359c199555a43572d76a
Cristian Vargas Scrummers <cristian.vargas@scrummers.co>
2024-11-28 12:03:00 -0500

    Solve logout when section is closed by error screen

--- lib/presentation/core/widgets/error/error_screen.dart
+++ lib/presentation/core/widgets/error/error_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:wallet_guru/domain/core/auth/auth_service.dart';
+import 'package:wallet_guru/infrastructure/core/routes/router_provider.dart';
 import 'package:wallet_guru/infrastructure/core/routes/routes.dart';
 import 'package:wallet_guru/presentation/core/widgets/layout.dart';
 import 'package:wallet_guru/presentation/core/widgets/text_base.dart';
 import 'package:wallet_guru/presentation/core/widgets/custom_button.dart';
 import 'package:wallet_guru/presentation/core/styles/schemas/app_color_schema.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ErrorScreen extends StatelessWidget {
   const ErrorScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final l10n = AppLocalizations.of(context)!;
+    final AuthService authService = AuthService();
     Size size = MediaQuery.of(context).size;
     return WalletGuruLayout(
       showBackButton: false,
@@ -26,7 +31,7 @@ class ErrorScreen extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             TextBase(
-              text: 'A problem occurred, try again later',
+              text: l10n.problemModal,
               fontSize: size.width * 0.04,
               fontWeight: FontWeight.w500,
               textAlign: TextAlign.center,
@@ -36,9 +41,12 @@ class ErrorScreen extends StatelessWidget {
               width: size.width * 0.35,
               border: Border.all(
                   color: AppColorSchema.of(context).buttonBorderColor),
-              onPressed: () => GoRouter.of(context).go(
-                Routes.logIn.path,
-              ),
+              onPressed: () async {
+                await authService.logout();
+                GoRouter.of(navigatorKey.currentContext!).go(
+                  Routes.logIn.path,
+                );
+              },
               text: 'Ok',
               fontSize: 20,
               fontWeight: FontWeight.w400,
